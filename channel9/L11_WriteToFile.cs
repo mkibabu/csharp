5ad66a233cdb1a8dec3e1ed0789e828b83f41e25
Michael Muraya <mkibabu@gmail.com>
2013-11-05 17:11:39 -0500

    Add more ways to write to file

--- channel9/L11_WriteToFile.cs
+++ channel9/L11_WriteToFile.cs
@@ -11,7 +11,7 @@ namespace  WriteToFile
         public static void Main(string[] args)
         {
             // get the file name
-            string path = "l11Output";
+            string path = "l11Output.out";
 
             // delete the file if it exists
             if(File.Exists(path))
@@ -21,13 +21,33 @@ namespace  WriteToFile
 
             // Example 1# Write an array to a new file
             string[] lines = {"These lines written using WriteAllLines(path, array)", "Second line", "Third line"};
-            // WriteAllLines() creates a file, writes all the contents of a
+            // WriteAllLines creates a file, writes all the contents of a
             // collection to it, and closes the file.
             File.WriteAllLines(path, lines);
 
-            // Example 2: Append to file using WriteAllLines
+            // Example 2: Append to file using AppendAllLines
             string[] lines1 = {"These lines written using AppendAllLines(path, array)", "Appended line", "Other appended line"};
-            File.AppendAllText(path, lines1);
+			// AppendAllLines opens a file, writes all the contents of a
+			// collection to it, and closes the file
+            File.AppendAllLines(path, lines1);
+			
+			// Example 3: Write one string to a text file.
+			string text = "This single line of text was written using WriteText(path, text)";
+			// WriteAllText(path, text) creates the file and writes text to it.
+			// If path exists, the file is overwritten.
+			// Commented out to keep previous writes
+			
+			// File.WriteAllText(path, text);
+			
+			
+			// Example 4: Write only some strings in an array to a file.
+			// Twist; combine the two arrays above (line and line1). Then
+			// iterate N/2 times (where N = length of new array)
+			
+			
+			
+			Console.WriteLine("Hit any key to quit.");
+			Console.ReadLine();
         }
     }
 }
