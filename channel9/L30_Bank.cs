b848f5e797ddcef3987ba4e03ffb25f732599389
Michael Muraya <mkibabu@gmail.com>
2013-11-18 13:20:26 -0500

    Add lesson 30: Unit testing

---
+++ channel9/L30_Bank.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// The Woodgrove Bank sample project is a simple bank account project used to
+// generate unit tests. The code starts with an intentional error in the 
+// Debit(double) method, which has a + sign rather than the correct - sign.
+// Fixed as the unit tests project goes on.
+
+// Source: "Sample Project for creating unit tests", MSDN
+// http://msdn.microsoft.com/en-us/library/ms243176\(v=vs.110\).aspx
+
+namespace BankAccountNS
+{
+    /// <summary>
+    /// Bank account demo class
+    /// </summary>
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+
+        // variables used for error messages
+        public const string AmountLessThanZero = "Amount is less than zero";
+        public const string AmountMoreThanBalance = "Amount is more than available balance";
+
+        // good practice to always have a default contructor; however, to keep
+        // other classes from calling it, make it private.
+        private BankAccount()
+        {
+
+        }
+
+        // Main (and only) constructor
+        public BankAccount(string aCustomerName, double aBalance)
+        {
+            m_customerName = aCustomerName;
+            m_balance = aBalance;
+        }
+
+        // Properties. Makes the customerName and balance variables readonly,
+        // without needing the readonly keyword (which means variables must be
+        // initlalized in constructor or at declaration)
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance;  }
+        }
+
+        // private methods. Never repeat/copy-paste code
+        private void checkIfAccountFrozen()
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+        }
+
+        private void checkIfAmountInvalid(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Amount", amount, AmountLessThanZero);
+            }
+        }
+
+        private void freezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        private void unfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
+
+        //! Starts with a known error: amount should be subtracted from
+        //! balance, not added. Corrected as project progresses, but original
+        // erroneous line commented out.
+        public void Debit(double amount)
+        {
+            checkIfAccountFrozen();
+
+            checkIfAmountInvalid(amount);
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("Amount", amount, AmountMoreThanBalance);
+            }
+
+            // m_balance += amount;    // original intentionally erroneous code
+            m_balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            checkIfAccountFrozen();
+            
+            checkIfAmountInvalid(amount);
+
+            m_balance += amount;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}\nBalance: {1:C}\nFrozen? (Y/N): {2}",
+                this.CustomerName, this.Balance, m_frozen ? "Y" : "N");
+        }
+    }
+
+    public class BankMain
+    {
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine(ba.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ channel9/L30_UnitTests_Bank.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+// Unit Tests for the Bank solution.
+// Source: http://msdn.microsoft.com/en-us/library/ms182532(v=vs.110).aspx
+namespace BankUTest
+{
+    //! Test class requirements:
+    // The [TestClass] attribute, required by the Test Explorer.
+    // Each emthod being tested must have a [TestMethod] attibute.
+    [TestClass]
+    public class BankAccountUnitTests
+    {
+        // Test the ebit method. Four possible outcomes, i.e.
+        // 1. throw ArgumentOutOfRangeException if (debit amount) < 0
+        // 2. throw ArgumentOutOfRangeException if (debit amount) > balance
+        // 3. Throw Exception if (account is frozen)
+        // 4. if 1-3 pass, the balance should be reduced by the amount.
+
+        // Test method format:
+        // MethodUnderTest_TestCondition_RespondsInThisManner()
+        // { Arrange: Act; Assert }
+
+        //! First test: verify that a valid amount leads to a withdrawal
+        [TestMethod]
+        public void Debit_WithValidAmount_ReducesBalance()
+        {
+            // arrange
+            double startBalance = 11.99;
+            double debitAmount = 10.00;
+            double expectedBalance = 1.99;
+
+            BankAccount testAccount = new BankAccount("Mr. Cookie Monster", startBalance);
+
+            // act
+            testAccount.Debit(debitAmount);
+
+            // assert
+            double actualBalance = testAccount.Balance;
+            // verify that the two amounts are equal, or within some specified
+            // accuracy of one another.
+            Assert.AreEqual(expectedBalance, actualBalance, 0.001, "Account not debited correctly");
+        }
+
+        //! Second test: verify that an ArgumentOutOfRange exception is thrown
+        //! when the amount to withdraw is less than 0
+       
+
+        // Original unit test for amount less than 0. Adequate, but difficult to
+        // decipher which part of the exception throws the error. Adding the 
+        // costomized error messages to the test class, and altering the contructor
+        // of the exceptions thrown, allows us to narrow down which exception is
+        // thrown.
+
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
+        //public void Debit_WhenAmountIsLessThanZero_ThrowsArgumentOutOfRangeException()
+        //{
+        //    // arrange
+        //    double startBalance = 11.99;
+        //    double debitAmount = -100.00;
+
+        //    BankAccount testAccount = new BankAccount("Mr. Cookie Monster", startBalance);
+
+        //    // act
+        //    testAccount.Debit(debitAmount);
+
+        //    // assert
+        //    // Here, assert is handled by the ExpectedException attribute            
+        //}
+
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ThrowsArgumentOutOfRangeException()
+        {
+            // arrange
+            double startBalance = 11.99;
+            double debitAmount = -100.00;
+
+            BankAccount testAccount = new BankAccount("Mr. Cookie Monster", startBalance);
+
+            // act
+            try
+            {
+                testAccount.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.Contains(e.Message, BankAccount.AmountLessThanZero);
+                return;
+            }
+
+            // Code to run if ArgumentOutOfRangeException is not thrown.
+            Assert.Fail("ArgumentOutOfRangeException was not thrown!");
+        
+        }
+
+        //! Third test: verify that an ArgumentOutOfRange exception is thrown
+        //! when the amount to withdraw is greater then the balance
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ThrowsArgumentOutOfRangeException()
+        {
+            // arrange
+            double startBalance = 11.99;
+            double debitAmount = 100.00;
+
+            BankAccount testAccount = new BankAccount("Mr. Cookie Monster", startBalance);
+
+            try
+            {
+                // act
+                testAccount.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.Contains(e.Message, BankAccount.AmountMoreThanBalance);
+                return;
+            }
+
+            // Code run when ArgumentOutOfRangeException is not thrown.
+            Assert.Fail("ArgumentOutOfRangeException was not thrown!");
+        }
+    }
+}
