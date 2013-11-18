3a82c5b3cac6c8db50847bf21b20c78e062e8e02
Michael Muraya <mkibabu@gmail.com>
2013-11-18 18:18:17 -0500

    Add lesson 31: Conditional methods

---
+++ channel9/L31_ConditionalMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using L31_TraceFunctions;
+
+//! compile with: /reference: L31_ConditionalMethodsDLL.dll [/d:DEBUG]
+
+namespace L31_TraceClient
+{
+    public class TraceClient
+    {
+        public static void Main(string[] args)
+        {
+            Trace.Message("Main starting");
+
+            if (args.Length == 0)
+                Console.WriteLine("No arguments have been passed");
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                Console.WriteLine("Args[{0}] is {1}", i, args[i]);
+                }
+            }
+
+            Trace.Message("Main ending");
+        }
+    }
+}
---
+++ channel9/L31_ConditionalMethodsDLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+//! Compile with: /target:library /d:DEBUG
+
+// Conditional methods allow developers to create methods whose calls are placed
+// in code and either included or excluded during compilation based on a
+// preprocessing symbol. For instance, assertion/debugging code can be included
+// or excluded based on whther or not a preprocessing identifer is included. This
+// identifier can be added using #define within the code, or by using the C#
+// command line via the /define optoin (/d).
+
+namespace L31_TraceFunctions
+{
+    // creates the conditional method
+    public class Trace
+    {
+        // Mark the method as being conditional. The attribute takes the 
+        // preprocessing identifier as a parameter. The identifier detgermines
+        // whether the method call is included when clients are compiled.
+        [Conditional("DEBUG")]
+        public static void Message(string traceMessage)
+        {
+            Console.WriteLine("[TRACE] - " + traceMessage);
+        }
+    }
+   
+}
